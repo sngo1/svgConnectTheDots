3b431f37abfef123cd6fb9443be8bab186e87a07
sngo1 <sngo1@stuy.edu>
2018-03-01 06:15:21 -0500

    Dot is drawn

---
+++ svg.js
@@ -0,0 +1,63 @@
+var pic = document.getElementById("vimage");
+console.log("PIC: ", pic);
+var cl = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+console.log("cl: ", cl);
+
+/*
+cl.setAttribute("cx", "0");
+cl.setAttribute("cy", "0");
+cl.setAttribute("r", "200");
+cl.setAttribute("fill", "purple");
+cl.setAttribute("stroke", "pink");
+console.log("Attribute cx changed to 200");
+
+pic.appendChild(cl);
+console.log("Child appended.")
+*/
+/*
+var change = function(e){
+    pic = this;
+    this.setAttribute("fill","green");
+    console.log("Color changed to green.");
+}
+*/
+
+var clearBoard = function(){
+    console.log(pic);
+    objectsLeft = pic.children.length;
+    while (objectsLeft > 0){
+	console.log("Length: ", objectsLeft);
+	pic.removeChild(pic.children[0]);
+	console.log("Element Removed");
+	objectsLeft -= 1;
+    }
+}
+
+console.log("clearboard tests:");
+clearBoard()
+
+var drawDot = function(xCor, yCor){
+    cl.setAttribute("cx", String(xCor));
+    cl.setAttribute("cy", String(yCor));
+    cl.setAttribute("r", "20");
+    cl.setAttribute("fill", "pink");
+    cl.setAttribute("stroke", "pink");
+    console.log("Circle drawn.");
+    x = cl
+    pic.appendChild(x);
+    lastCor = [xCor, yCor];
+    return lastCor;
+}
+
+drawDot(30,30);
+console.log("Dot drawn");
+
+var clicked = function(e){
+    console.log("X: ", e.offsetX);
+    console.log("Y: ", e.offsetY);
+    
+    drawDot(e.offsetX, e.offsetY);
+}
+
+pic.addEventListener("click", clicked);
+console.log(pic.children);
